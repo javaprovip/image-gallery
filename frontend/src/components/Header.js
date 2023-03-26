82cd64b0d150aedf96a1465659ac2d5fb3be9ded
Sergey Kravchenko <sergiokravchenko@gmail.com>
2023-03-26 17:09:05 +0300

    lint changed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,16 +3,16 @@ import { Navbar, Container } from 'react-bootstrap';
 import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-    backgroundColor: 'lightgreen',
+  backgroundColor: 'lightgreen',
 };
 
 const Header = ({ title }) => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
+      </Container>
+    </Navbar>
+  );
 };
 export default Header;
